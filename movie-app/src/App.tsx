6ddf5d1a7316d2699671aec2074dcdf4afac0404
souma <souma3789522@gmail.com>
2025-09-25 18:08:05 +0900

    complete chapter6

---
+++ movie-app/src/App.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+type Movie = {
+  id: string;
+  original_title: string;
+  poster_path: string;
+  overview: string;
+};
+
+type MovieJson = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+function App() {
+  const fetchMovieList = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=ja&page=1`,
+      {
+        headers: {
+          Authorization : `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+        },
+      }
+    );
+    const data = await response.json();
+    setMovieList(
+      data.results.map((movie: Movie) => ({
+        id: movie.id,
+        original_title: movie.original_title,
+        poster_path: movie.poster_path,
+        overview: movie.overview
+      }))
+    );  
+  };
+
+  const [keyword, setKeyword] = useState("");
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    fetchMovieList();
+  }, []);
+
+  return (
+    <div>
+      <div>{keyword}</div>
+      <input type="text" onChange={(e) => setKeyword(e.target.value)}/>
+      {movieList
+        .filter((movie) => movie.original_title.includes(keyword))
+        .map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.original_title}</h2>
+            <img 
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.original_title}
+            />
+            <p>{movie.overview}</p>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default App
